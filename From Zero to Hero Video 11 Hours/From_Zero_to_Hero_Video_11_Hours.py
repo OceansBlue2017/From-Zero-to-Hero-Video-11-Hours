13a1cfbd5fd6e6fc205a6aa7afa15424adf6caf0
OceansBlue <33443506+OceansBlue2017@users.noreply.github.com>
2018-12-16 12:43:50 -0600

    new lines added

---
+++ From Zero to Hero Video 11 Hours/From_Zero_to_Hero_Video_11_Hours.py
@@ -0,0 +1,11 @@
+print ('its a beautiful day in Keller')
+
+print ("It's a sunny day,  \n  in Keller")  #  \n prints in a new line 
+print (""" This is another way to print 
+            over multiple lines""")
+print ('Here is a double quote " ' + "Here is a single quote ' " ) # print the double quote +  single quote
+
+print('Or we can put it like this \" to get the double quote to print')
+
+print ("It's a sunny day,  \\news   in Keller")  #  \\ before the n to print the n letter 
+
